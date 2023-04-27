6cf408faa04552995e7cd5ec63a6ab98197e50f2
Alina Dubrovina <dubrovinaa389@gmail.com>
2023-04-27 15:57:07 +0200

    feat: add SavedVoteDTO class

--- src/main/java/by/it_academy/vote/core/dto/SavedVoteDTO.java
+++ src/main/java/by/it_academy/vote/core/dto/SavedVoteDTO.java
@@ -1,36 +1,29 @@
 package by.it_academy.vote.core.dto;
 
+
 import java.time.LocalDateTime;
 import java.util.Objects;
 
 public class SavedVoteDTO {
+    private VoteDTO voteDTO;
     private LocalDateTime dtCreate;
-    private VoteDTO vote;
 
-    public SavedVoteDTO(LocalDateTime dtCreate, VoteDTO vote) {
-        this.dtCreate = dtCreate;
-        this.vote = vote;
+    public SavedVoteDTO(VoteDTO voteDTO, LocalDateTime localDateTime) {
+        this.voteDTO = voteDTO;
+        this.dtCreate = localDateTime;
     }
 
-    public SavedVoteDTO(VoteDTO vote) {
-        this.dtCreate = LocalDateTime.now();
-        this.vote = vote;
+    public SavedVoteDTO(VoteDTO voteDTO) {
+        this.voteDTO = voteDTO;
+        dtCreate = LocalDateTime.now();
+    }
+
+    public VoteDTO getVoteDTO() {
+        return voteDTO;
     }
 
     public LocalDateTime getDtCreate() {
         return dtCreate;
-    }
-
-    public void setDtCreate(LocalDateTime dtCreate) {
-        this.dtCreate = dtCreate;
-    }
-
-    public VoteDTO getVote() {
-        return vote;
-    }
-
-    public void setVote(VoteDTO vote) {
-        this.vote = vote;
     }
 
     @Override
@@ -38,19 +31,19 @@ public class SavedVoteDTO {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
         SavedVoteDTO that = (SavedVoteDTO) o;
-        return Objects.equals(dtCreate, that.dtCreate) && Objects.equals(vote, that.vote);
+        return Objects.equals(voteDTO, that.voteDTO) && Objects.equals(dtCreate, that.dtCreate);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(dtCreate, vote);
+        return Objects.hash(voteDTO, dtCreate);
     }
 
     @Override
     public String toString() {
         return "SavedVoteDTO{" +
-                "dtCreate=" + dtCreate +
-                ", vote=" + vote +
+                "voteDTO=" + voteDTO +
+                ", dtCreate=" + dtCreate +
                 '}';
     }
 }
